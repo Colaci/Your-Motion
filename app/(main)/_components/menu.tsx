3c7892560078d07edcdb7a59644fbef22a26a80b
wy <1311936071@qq.com>
2024-01-29 18:46:28 +0800

    feat: icon and cover image added

--- app/(main)/_components/menu.tsx
+++ app/(main)/_components/menu.tsx
@@ -5,12 +5,17 @@ import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
-  DropdownMenuSeparator
+  DropdownMenuSeparator,
+  DropdownMenuItem
  } from "@/components/ui/dropdown-menu";
 import { useUser } from "@clerk/clerk-react";
 import { useRouter } from "next/navigation";
 import { api } from "@/convex/_generated/api";
 import { useMutation } from "convex/react";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { MoreHorizontal, Trash } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 interface MenuProps{
   documentId: Id<"documents">;
 }
@@ -19,10 +24,36 @@ export const Menu = ({documentId}:MenuProps)=>{
   const {user} = useUser()
 
   const archive = useMutation(api.documents.archive)
+  const onArchive = ()=>{
+    const promise = archive({id:documentId})
+    toast.promise(promise,{
+      loading: "Moving to trash...",
+      success:"Note moved to trash!",
+      error: "Failed to archive note."
+    })
+  }
   
   return (
-    <div>
-      Menu
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button size="sm" variant="ghost">
+          <MoreHorizontal className="h-4 w-4"/>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60" align="end" alignOffset={8} forceMount>
+        <DropdownMenuItem onClick={onArchive}>
+          <Trash className="h-4 w-4 mr-2"/>
+          Delete
+        </DropdownMenuItem>
+        <DropdownMenuSeparator/>
+        <div className="text-xs text-muted-foreground p-2">
+          Last edited by: {user?.username}
+        </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
+}
+// 5:44
+Menu.Skeleton = function MenuSkeleton(){
+  return <Skeleton className="h-10 w-10"/>
 }
